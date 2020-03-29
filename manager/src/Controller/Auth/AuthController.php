a290a2224792cfd27162e3630f6c8dfc1b420953
Nicolay Poymanov <n.poymanov@gmail.com>
2020-03-29 15:40:04 +0300

    Добавлен контроллер аутентификации

---
+++ manager/src/Controller/Auth/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('app/auth/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     * @throws Exception
+     */
+    public function logout()
+    {
+        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+}

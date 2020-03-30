162d6fd321a2c4be7e1adafd7a0bb36d14ca6a5a
poymanov <n.poymanov@gmail.com>
2020-03-30 20:01:25 +0300

    Добавлена консольная команда изменения роли пользователя

--- manager/src/Model/User/UseCase/Role/Command.php
+++ manager/src/Model/User/UseCase/Role/Command.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace App\Model\User\UseCase\Role;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
     /**
      * @var string
+     * @Assert\NotBlank()
      */
     public $id;
 
     /**
      * @var string
+     * @Assert\NotBlank()
      */
     public $role;
+
+    /**
+     * @param string $id
+     */
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
 }

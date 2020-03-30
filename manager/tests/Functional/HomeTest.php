573a1954856ed64dd274e530bc28abeeca0a02ba
poymanov <n.poymanov@gmail.com>
2020-03-30 14:02:46 +0300

    Добавлено функциональное тестирование главной страницы

---
+++ manager/tests/Functional/HomeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeTest extends WebTestCase
+{
+    public function testGuest(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+        $this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
+    }
+
+    public function testSuccess(): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'admin@app.test',
+            'PHP_AUTH_PW' => 'password',
+        ]);
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertContains('Hello', $crawler->filter('h1')->text());
+    }
+}

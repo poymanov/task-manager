b6d53b71f2a27d05e4feab21e551b4d24dd387d5
poymanov <n.poymanov@gmail.com>
2020-04-07 22:51:52 +0300

    Добавлен сервис управления доступом к задачам

--- manager/src/Model/Work/Entity/Projects/Role/Permission.php
+++ manager/src/Model/Work/Entity/Projects/Role/Permission.php
@@ -9,6 +9,8 @@ use Webmozart\Assert\Assert;
 class Permission
 {
     public const MANAGE_PROJECT_MEMBERS = 'manage_project_members';
+    public const VIEW_TASKS = 'view_tasks';
+    public const MANAGE_TASKS = 'manage_tasks';
 
     private $name;
 
@@ -27,7 +29,9 @@ class Permission
     public static function names(): array
     {
         return [
-            self::MANAGE_PROJECT_MEMBERS
+            self::MANAGE_PROJECT_MEMBERS,
+            self::VIEW_TASKS,
+            self::MANAGE_TASKS,
         ];
     }
 
---
+++ manager/src/Security/Voter/Work/Projects/TaskAccess.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Voter\Work\Projects;
+
+use App\Model\Work\Entity\Members\Member\Id;
+use App\Model\Work\Entity\Projects\Role\Permission;
+use App\Model\Work\Entity\Projects\Task\Task;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class TaskAccess extends Voter
+{
+    public const VIEW = 'view';
+    public const MANAGE = 'edit';
+    public const DELETE = 'delete';
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $security;
+
+    /**
+     * @param AuthorizationCheckerInterface $security
+     */
+    public function __construct(AuthorizationCheckerInterface $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, [self::VIEW, self::MANAGE, self::DELETE]) && $subject instanceof Task;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return
+                    $this->security->isGranted('ROLE_WORK_MANAGE_PROJECTS') ||
+                    $subject->getProjets()->isMemberGranted(new Id($user->getId()), Permission::VIEW_TASKS);
+            case self::MANAGE:
+                return
+                    $this->security->isGranted('ROLE_WORK_MANAGE_PROJECTS') ||
+                    $subject->getProjets()->isMemberGranted(new Id($user->getId()), Permission::MANAGE_TASKS_TASKS);
+            case self::DELETE:
+                return
+                    $this->security->isGranted('ROLE_WORK_MANAGE_PROJECTS');
+                break;
+        }
+
+        return false;
+    }
+}
